80f72d9b13c82af1ee8134fab0348902f013d006
nishitaku <29690066+nishitaku@users.noreply.github.com>
2022-01-03 14:08:05 +0900

    add root store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AppStoreModule } from './app-store/app-store.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, AppStoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
