47831481f07da8de91d452c51a50bb5491f17c20
nishitaku <29690066+nishitaku@users.noreply.github.com>
2022-01-03 16:49:26 +0900

    add ngrx boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AppStoreModule } from './app-store/app-store.module';
+import { TodoModule } from './todo/todo.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, AppStoreModule],
+  imports: [BrowserModule, AppRoutingModule, AppStoreModule, TodoModule],
   providers: [],
   bootstrap: [AppComponent],
 })
