3131e79e63f7648bb22b94acc2c51ed8361cb55a
nishitaku <29690066+nishitaku@users.noreply.github.com>
2022-01-03 23:29:15 +0900

    implements todo facade

---
+++ src/app/todo/state/todo.facade.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { TodoModule } from '../todo.module';
+import * as TodoSelectors from './todo.selectors';
+import * as TodoActions from './todo.actions';
+
+@Injectable({
+  providedIn: TodoModule,
+})
+export class TodoFacade {
+  loading$ = this.store.pipe(select(TodoSelectors.getLoading));
+  todos$ = this.store.pipe(select(TodoSelectors.getTodos));
+
+  constructor(private store: Store) {}
+
+  loadAll(offset?: number, limit?: number) {
+    this.store.dispatch(TodoActions.loadAll({ offset, limit }));
+  }
+
+  load(id: number) {
+    this.store.dispatch(TodoActions.load({ id }));
+  }
+}
