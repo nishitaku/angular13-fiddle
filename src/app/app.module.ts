a246f597599aab752e3db5cb46553d8c9f1b9dc1
nishitaku <29690066+nishitaku@users.noreply.github.com>
2022-01-19 08:59:57 +0900

    move shared store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AppStoreModule } from './app-store/app-store.module';
+import { AppStoreModule } from './shared/store/app-store.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
